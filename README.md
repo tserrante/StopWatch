2c90e387b0f7d993a4acb6ed5e91cb86d9766c5f
tserrante <tserrante@gmail.com>
2022-02-28 20:28:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A simple JavaScript stopwatch using OOP techniques
+
+- The stopwatch is implemented as an object literal
+
+- To Do
+
+1. implement defineProperties instead of multiple define property
---
+++ script.js
@@ -0,0 +1,81 @@
+
+// Stop Watch using JS objects
+
+function StopWatch(milliSeconds, seconds, minutes, hours)
+{
+    let milliSeconds, seconds, minutes, hours;
+
+    // define milliSeconds
+    Object.defineProperty(this, 'milliSeconds', {
+        get: function(){return milliSeconds;},
+        set: function(value)
+        {
+            if(!value || value < 0) 
+                throw new Error('Invalid time (milliseconds)');
+            else
+                milliSeconds = value;
+        }
+    })
+    // define seconds
+    Object.defineProperty(this, 'seconds', {
+        get: function(){return seconds;},
+        set: function(value)
+        {
+            if(!value || value < 0 ) 
+                throw new Error('Invalid time (seconds)');
+            else
+                seconds = value;
+        }
+    })
+    // define minutes
+    Object.defineProperty(this, 'minutes', {
+        get: function(){return minutes;},
+        set: function(value)
+        {
+            if(!value || value < 0) 
+                throw new Error('Invalid time (minutes)');
+            else
+                minutes = value;
+        }
+    })
+    // define milliSeconds
+    Object.defineProperty(this, 'hours', {
+        get: function(){return hours;},
+        set: function(value)
+        {
+            if(!value || value < 0) 
+                throw new Error('Invalid time (hours)');
+            else
+                hours = value;
+        }
+    })
+
+    function getTime()
+    {
+        console.log(hours + " : " + minutes + " : " + seconds + " : " + milliSeconds);
+    }
+
+    function start()
+    {
+        // starts the watch
+    }
+
+    function stop()
+    {
+        // stops the watch
+    }
+
+    function duration()
+    {
+        // displays the duration after both stop and start
+    }
+
+    function reset()
+    {
+        // resets the time
+        milliSeconds.set = 0;
+        seconds.set = 0;
+        minutes.set = 0;
+        hours.set = 0;
+    }
+}
